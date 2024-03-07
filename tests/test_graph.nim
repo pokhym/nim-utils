038a48466bf5126e8ae4c7d70a904277a14788b3
pokhym <ng121@purdue.edu>
2024-03-07 18:53:06 -0500

    add test for non-existent node edge adding

--- tests/test_graph.nim
+++ tests/test_graph.nim
@@ -20,6 +20,21 @@ proc test2() =
   echo "Printing dg..."
   echo $dg
 
+proc test3() =
+  ## Add edges beteen non-existing nodes
+  echo "Running test3..."
+  let udg: Graph[int] = newGraph[int](false)
+  let dg: Graph[int] = newGraph[int](true)
+
+  assert udg.insertNode(0, 1) == true
+  assert dg.insertNode(0, 1) == true
+
+  assert udg.addEdge(0, 2) == false
+  assert udg.addEdge(2, 2) == false
+  assert dg.addEdge(0, 2) == false
+  assert dg.addEdge(2, 2) == false
+
 when isMainModule:
   test1()
   test2()
+  test3()
